8a077429a67c97e208cd233dc8b980afbebb955b
Ankit Kr. Pandey <ankit.pandey@unthinkable.co>
2023-02-20 16:09:02 +0530

    HealthKitSDK slidebar creation according to figma deaign

--- app/src/main/java/com/example/healthkitsample/SignupActivity.java
+++ app/src/main/java/com/example/healthkitsample/SignupActivity.java
@@ -1,8 +1,12 @@
 package com.example.healthkitsample;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.material.slider.Slider;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        Slider seekBar = findViewById(R.id.slideBar);
+        seekBar.addOnSliderTouchListener(new Slider.OnSliderTouchListener() {
+            @Override
+            public void onStartTrackingTouch(@NonNull Slider slider) {
+                int value = (int) seekBar.getValue();
+                Log.d("Slider Value", String.valueOf(value));
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(@NonNull Slider slider) {
+                int value = (int) seekBar.getValue();
+                Log.d("Slider Value 2", String.valueOf(value));
+
+            }
+        });
     }
 }
